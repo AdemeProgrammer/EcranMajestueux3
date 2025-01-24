98b2cbe44479e84013a380ab61ba1f48aabeaa62
Ademe <A.AYAD@lprs.fr>
2025-01-24 11:52:10 +0100

    [feat] Ajoutmodif + supp (non fonctionnelle)
    Ajout des pages de modifications et de suppression (non
    fonctionnelles).

---
+++ ModificationEM.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+$modification = new PDO('mysql:host=localhost;dbname=ecranmajestueux', 'root', '');
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+    $mot_de_passe = $_POST['mot_de_passe'];
+
+    $req = $modification -> prepare('UPDATE inscrit SET nom = :nom, prenom = :prenom, mot_de_passe = :mot_de_passe WHERE id = :id');
+    $req -> execute(array(
+        'nom' => $nom,
+        'prenom' => $prenom,
+        'mot_de_passe' => $mot_de_passe
+    ));
+
+    echo "Votre compte a bien été modifié !";
+    header('Location: ConnexionRV.php');
+
+}
+
+?>
+
+<html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Modification</title>
+    <link href="ModificationSuppressionEM.css" rel="stylesheet">
+</head>
+<body>
+<hr>
+<h1>Modification du compte de : <?= $_SESSION['email']?></h1>
+<hr>
+
+<form action ="ModificationEM.php" method="post">
+    <p>Modifier le nom :</p>
+    <input type="text" name="nom">
+    <p>Modifier le prenom :</p>
+    <input type="text" name="prenom">
+    <p>Modifier le mot de passe :</p>
+    <input type="password" name="mot_de_passe">
+    <input type = "submit" name ="validation">
+</form>
+</body>
+</html>
