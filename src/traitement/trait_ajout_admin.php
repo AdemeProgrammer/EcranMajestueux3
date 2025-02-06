5538f7e4a13d86312efe7a299e4f4bf6eb0233b2
Ademe <A.AYAD@lprs.fr>
2025-02-06 16:52:26 +0100

    [feat] Ajout d'une inscription admin
    Ajout d'une inscription admin (pas fonctionelle) : la personne peut
    s'inscrire mais le rôle ne se met pas. ALED !!!!!!!!!!!!!!!

---
+++ src/traitement/trait_ajout_admin.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once '../bdd/Bdd.php';
+require_once '../../vue/InscriptionEM.php';
+require_once '../modele/Utilisateurs.php';
+require_once '../Repository/UtilisateursRepository.php';
+
+if (empty($_POST["nom"]) ||
+    empty($_POST["prenom"]) ||
+    empty($_POST["email"]) ||
+    empty($_POST["mot_de_passe"])) {
+
+    echo "C'est pas bien ...";
+    header("Location: ../../vue/InscriptionEM.php");
+} else {
+    $utilisateurs = new Utilisateurs([
+        'nom' => $_POST['nom'],
+        'prenom' => $_POST["prenom"],
+        'email' => $_POST["email"],
+        'motDePasse' => $_POST["mot_de_passe"],
+        'role' => 'Administrateur',
+    ]);
+    $UtilisateursRepository = new UtilisateursRepository();
+    $resultat = $UtilisateursRepository->ajoutUtilisateurs($utilisateurs);
+
+    if ($resultat == true) {
+        header("Location: ../../vue/ConnexionEM.php");
+    } else {
+        header("Location: ../../vue/InscriptionEM.php");
+    }
+
+}
--- vue/InscriptionAdminEM.php
+++ vue/InscriptionAdminEM.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inscription</title>
+    <title>Inscription Admin</title>
     <link href="../assets/css/InscriptionConnexionEM.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 
 
 </head>
-<body>
+<>
 
 <div class="imagedefond">
     <h2 class="form-title">Inscription</h2>
@@ -46,8 +46,6 @@
 </div>
 <a href="ConnexionEM.php">Je suis déjà membre</a>
 <br>
-<a href="InscriptionAdminEM.php">Créer un compte admin</a>
+<a href="InscriptionEM.php">Je veux créer un compte client</a>
 </body>
 </html>
-
- 
