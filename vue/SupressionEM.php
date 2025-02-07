9e27d197234cb5f511f6a45949037300fb339556
Tom TREILLON <TREILLON_To@lprs.local>
2025-02-07 11:15:52 +0100

    [feat] Ajout des mentions légales et amélioration supprétion

--- vue/SupressionEM.php
+++ vue/SupressionEM.php
@@ -1,15 +1,3 @@
-<?php
-session_start();
-if (isset($_POST['mot_de_passe'])) {
-    $affirmsup = $_POST['suppression'];
-    if ($affirmsup == "SUPPRESSION DE COMPTE") {
-        $suppression = new PDO('mysql:host=localhost;dbname=ecranmajestueux', 'root', '');
-        $req = $suppression->prepare('DELETE * FROM utilisateurs WHERE id_utilisateur=:id_utilisateur');
-        $req->execute(array('id_utilisateur' => $_POST['id_utilisateur']));
-        header("location:ConnexionEM.php");
-    }
-}
-?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -21,7 +9,7 @@ if (isset($_POST['mot_de_passe'])) {
 <div class="container">
     <h1>Suppression de Compte</h1>
     <p>Êtes-vous sûr de vouloir supprimer votre compte ? Cette action est irréversible.</p>
-    <form action="/delete-account" method="POST">
+    <form action="../src/traitement/trait_supp.php" method="POST">
         <button type="submit">Supprimer mon compte</button>
     </form>
 </div>
