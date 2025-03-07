1b37230df8aa9f11364e2322028b925f03017ebc
Tom TREILLON <TREILLON_To@lprs.local>
2025-03-07 08:54:25 +0100

    [fix]Page d'admin

--- vue/AdminEM.php
+++ vue/AdminEM.php
@@ -6,11 +6,14 @@
 
 </head>
 <body>
-<a href="AjoutFilmEM.php">Ajouter un nouveau film</a>
+<p>Ici est le moyen pour vous d'ajouter un film à la liste</p><br>
+<a href="AjoutFilmEM.php">Ajouter un nouveau film</a><br><br>
 
-<a href="SceanceEM.php">Ajout de scéances</a>
+<p>Ici est le moyen pour vous d'ajouter des scéances à un film de la liste</p><br>
+<a href="SceanceEM.php">Ajout de scéances</a><br><br>
 
-<a href="ModificationEM.php">Modification utilisateurs</a>
+<p>Ici est le moyen pour vous de modifier les informations d'un utilisateur</p><br>
+<a href="ModificationEM.php">Modification utilisateurs</a><br><br>
 </body>
 
 </html>
