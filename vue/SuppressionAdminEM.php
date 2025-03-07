0c8117f2669d80bf5d44b6363e1c02b0ae50367a
Ademe <A.AYAD@lprs.fr>
2025-03-07 09:55:56 +0100

    [fix] suppr Admin

--- vue/SuppressionAdminEM.php
+++ vue/SuppressionAdminEM.php
@@ -11,7 +11,9 @@ session_start();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
+    <form action ="../src/traitement/trait_supp_admin.php" method="post">
     <input type="submit" value="Supprimer le compte">
+    </form>
+
 </body>
-</form>
 </html>
