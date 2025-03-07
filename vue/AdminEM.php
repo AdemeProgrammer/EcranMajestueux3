39064a310e00a7d255a008b926b859f66cfa4923
Tom TREILLON <TREILLON_To@lprs.local>
2025-03-07 07:42:11 +0100

    [feat]Ajout admin

---
+++ vue/AdminEM.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Page administrateur</title>
+
+</head>
+<body>
+<a href="AjoutFilmEM.php">Ajouter un nouveau film</a>
+
+<a href="SceanceEM.php">Ajout de scéances</a>
+
+<a href="ModificationEM.php">Modification utilisateurs</a>
+</body>
+
+</html>
